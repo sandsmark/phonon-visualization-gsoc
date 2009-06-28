84e31949aa96c85310253c740f29bfd72e115fb9
Martin Sandsmark <sandsmark@samfundet.no>
2009-06-29 01:54:47 +0200

    clean up the audiodataoutput for xine

--- xine/audiodataoutput.cpp
+++ xine/audiodataoutput.cpp
@@ -35,9 +35,14 @@ AudioDataOutputXT::AudioDataOutputXT(AudioDataOutput *output) :
 
 {
     m_xine = Backend::xine();
+
+    // Dummy audio input port, until we get the proper one
     m_audioPort = xine_open_audio_driver(m_xine, "none", 0);
 
+    // Allocate a new scope plugin
     m_plugin = (scope_plugin_t*)qMalloc(sizeof(scope_plugin_t));
+
+    // It is also a post plugin
     post_plugin_t *post_plugin  = (post_plugin_t*)m_plugin;
 
     {
@@ -47,6 +52,7 @@ AudioDataOutputXT::AudioDataOutputXT(AudioDataOutput *output) :
         //1 audio input, 0 video inputs
         _x_post_init(post_plugin, 1, 0);
 
+        // Populate the port with dummy functions
         post_audio_port_t *port = _x_post_intercept_audio_port(post_plugin, m_audioPort, &input, &output);
 
         if (!port) {
@@ -55,11 +61,15 @@ AudioDataOutputXT::AudioDataOutputXT(AudioDataOutput *output) :
             return;
         }
 
+        // Put in our own callbacks
         port->new_port.open       = openPort;
         port->new_port.close      = closePort;
         port->new_port.put_buffer = putBufferCallback;
+
+        // Store the audio port for future use
         m_audioPort = &port->new_port;
 
+        // Wire in the port input into our post plugin
         post_plugin->xine_post.audio_input[0] = &port->new_port;
         post_plugin->xine_post.type = PLUGIN_POST;
     }
@@ -71,13 +81,16 @@ AudioDataOutputXT::AudioDataOutputXT(AudioDataOutput *output) :
     post_plugin->running_ticket = (*m_xine).port_ticket;
     post_plugin->xine = m_xine;
 
+    // Store a reference to our own object in the post plugin struct
     m_plugin->audioDataOutput = this;
 }
 
 AudioDataOutputXT::~AudioDataOutputXT()
 {
+    // Let Xine dispose of our post plugin
     ((post_plugin_t*)m_plugin)->dispose((post_plugin_t*)m_plugin);
 
+    // Free the memory held by our plugin
     delete m_plugin;
 }
 
@@ -86,10 +99,14 @@ void AudioDataOutputXT::rewireTo(SourceNodeXT *source)
 {
     debug() << Q_FUNC_INFO << "rewiring to " << source;
     if (!source->audioOutputPort()) { // I can't get no satisfaction
+        debug() << Q_FUNC_INFO << " no audioport in source";
         return;
     }
+
+    // Make sure the source is sane
     source->assert();
 
+    // Get the audio input port in our post plugin
     xine_post_in_t *target = (xine_post_in_t*)xine_post_input(
                                     &((post_plugin_t*)m_plugin)->xine_post,
                                     const_cast<char*>("audio in"));
@@ -99,6 +116,7 @@ void AudioDataOutputXT::rewireTo(SourceNodeXT *source)
         return;
     }
 
+    // Make sure things went okay
     source->assert();
     SinkNodeXT::assert();
 }
@@ -106,6 +124,7 @@ void AudioDataOutputXT::rewireTo(SourceNodeXT *source)
 /// Returns this Source's audio output port
 xine_post_out_t *AudioDataOutputXT::audioOutputPort() const
 {
+    // Get our post plugin's audio output
     return xine_post_output(&((post_plugin_t)m_plugin->post).xine_post,
                             const_cast<char*>("audio out"));
 }
@@ -117,7 +136,9 @@ int AudioDataOutputXT::openPort(xine_audio_port_t *port_gen,
                                 uint32_t rate,
                                 int mode)
 {
+    // Reference to the relevant object
     AudioDataOutputXT *that = ((scope_plugin_t*)((post_audio_port_t*)port_gen)->post)->audioDataOutput;
+
     post_audio_port_t *port = (post_audio_port_t*)port_gen;
 
     _x_post_rewire((post_plugin_t*)port->post);
@@ -128,6 +149,7 @@ int AudioDataOutputXT::openPort(xine_audio_port_t *port_gen,
     port->rate = rate;
     port->mode = mode;
 
+    // Set the new audio stream parameters
     that->m_channels = _x_ao_mode2channels(mode);
     that->m_frontend->setChannels(that->m_channels);
     that->m_frontend->m_sampleRate = rate;
@@ -141,6 +163,7 @@ void AudioDataOutputXT::closePort(xine_audio_port_t *port_gen, xine_stream_t *st
     debug() << Q_FUNC_INFO << " closing port " << port_gen;
     post_audio_port_t *port = (post_audio_port_t*)port_gen;
 
+    // This is the same as closing the port, according to comments in the Xine source
     port->stream = NULL;
     port->original_port->close(port->original_port, stream);
 
@@ -152,15 +175,20 @@ void AudioDataOutputXT::putBufferCallback(xine_audio_port_t * port_gen, audio_bu
 {
     AudioDataOutputXT *that = ((scope_plugin_t*)((post_audio_port_t*)port_gen)->post)->audioDataOutput;
 
+    // Get the number of samples (audio frames * audio channels)
     int samples = buf->num_frames * that->m_channels;
 
+    // Store the audio data in a QVector
     QVector<qint16> buffer(samples);
     for (int i=0; i<samples; ++i)
         buffer[i] = buf->mem[i];
 
+    // Present the audio data to our frontend
     that->m_frontend->packetReady(buffer);
 
-    // Pass on the data to the audio output port
+    /* Send the audio buffer back to the original port.
+       This notifies Xine that we have finished processing
+       this buffer, so Xine can give us a new one */
     post_audio_port_t *port = (post_audio_port_t*)port_gen;
     port->original_port->put_buffer(port->original_port, buf, stream);
 }
@@ -180,22 +208,24 @@ AudioDataOutput::~AudioDataOutput()
     delete xt;
 }
 
-typedef QMap<Phonon::AudioDataOutput::Channel, QVector<float> > FloatMap;
-typedef QMap<Phonon::AudioDataOutput::Channel, QVector<qint16> > IntMap;
-
 inline void AudioDataOutput::packetReady(const QVector<qint16> buffer)
 {
-    //TODO: support floats
-    m_pendingData += buffer;
-
+    //TODO: support floats, we currently only handle ints
     if (m_format == Phonon::AudioDataOutput::FloatFormat)
         return;
 
+    // Add the new audio data to our pool
+    m_pendingData += buffer;
+
+    // While we have enough audio data for one signal
     while (m_pendingData.size() / m_channels > dataSize())
     {
+        QMap<Phonon::AudioDataOutput::Channel, QVector<qint16> > map;
+
+        /* If there's only one channel, we duplicate the data
+           into both channels in the QMap */
         if (m_channels==1)
         {
-            IntMap map;
             QVector<qint16> data = m_pendingData.mid(0, dataSize());
             m_pendingData.remove(0, dataSize());
             map.insert(Phonon::AudioDataOutput::LeftChannel, data);
@@ -204,14 +234,20 @@ inline void AudioDataOutput::packetReady(const QVector<qint16> buffer)
         }
         else if (m_channels==2)
         {
-            IntMap map;
             QVector<qint16> left, right;
-            for (int i=0; i < dataSize(); i+=2)
+
+            /* Copy out the interleaved data into the
+               appropriate channels.
+               This isn't very optimal. */
+            for (int i=0; i < dataSize()*2; i+=2)
             {
                 left.append(m_pendingData[i]);
                 right.append(m_pendingData[i+1]);
             }
+            // Remove the data we just copied out
             m_pendingData.remove(0, dataSize() * 2);
+
+            // Insert into the map, and emit
             map.insert(Phonon::AudioDataOutput::LeftChannel, left);
             map.insert(Phonon::AudioDataOutput::RightChannel, right);
             emit dataReady(map);
